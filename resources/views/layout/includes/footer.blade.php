e522b531e6d0c50e4d3ac54ae2133e7753fff35a
alexandrepalmag <alepalmag85ti@gmail.com>
2021-06-24 21:24:03 -0300

    SQL database scheme file saved.

--- resources/views/layout/includes/footer.blade.php
+++ resources/views/layout/includes/footer.blade.php
@@ -3,12 +3,12 @@
        <script type="text/javascript">
            document.addEventListener('DOMContentLoaded', function() {
                var elems = document.querySelectorAll('.autocomplete');
-               var instances = M.Autocomplete.init(elems, options);
+               var instances = M.Autocomplete.init(elems);
            });
 
            document.addEventListener('DOMContentLoaded', function() {
                var elems = document.querySelectorAll('.sidenav');
-               var instances = M.Sidenav.init(elems, options);
+               var instances = M.Sidenav.init(elems);
            });
        </script>
        </body>
--- resources/views/layout/includes/head.blade.php
+++ resources/views/layout/includes/head.blade.php
@@ -14,19 +14,18 @@
 </head>
 
 <body>
-    <header>
-        <nav>
-            <div class="nav-wrapper cyan darken-4">
-                <a href="{{route('admin.courses')}}" class="brand-logo">LaraCourses</a>
-                <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
-                <ul class="right hide-on-med-and-down">
-                    <li><a href="/">Home</a></li>
-                    <li><a href="{{route('admin.courses')}}">Courses</a></li>
-                </ul>
-            </div>
-        </nav>
-        <ul class="sidenav" id="mobile-demo">
-            <li><a href="/">Home</a></li>
-            <li><a href="{route{'admin.courses'}}">Courses</a></li>
-        </ul>
-    </header>
+
+    <nav>
+        <div class="nav-wrapper cyan darken-4">
+            <a href="{{ route('admin.courses') }}" class="brand-logo">LaraCourses</a>
+            <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+            <ul class="right hide-on-med-and-down">
+                <li><a href="/">Home</a></li>
+                <li><a href="{{ route('admin.courses') }}">Courses</a></li>
+            </ul>
+        </div>
+    </nav>
+    <ul class="sidenav" id="mobile-demo">
+        <li><a href="/">Home</a></li>
+        <li><a href="{{ route('admin.courses') }}">Courses</a></li>
+    </ul>
